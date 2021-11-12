aa039f0c0a173021b3971a16ba9568d774f2382c
Jeferson <jroliveira.oliveira301@gmail.com>
2021-11-12 17:59:33 +0100

    sm6150-common: Nuke trust service
    Change-Id: I5e8eb215c8eb96e25720e38c971c901e30066c6c

--- common.mk
+++ common.mk
@@ -506,10 +506,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.thermal@2.0-service.qti
 
-# Trust HAL
-PRODUCT_PACKAGES += \
-    vendor.lineage.trust@1.0-service
-
 # Update engine
 PRODUCT_PACKAGES += \
     update_engine \
