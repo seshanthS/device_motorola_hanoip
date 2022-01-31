3c129334608ead9f43bb33ecf2e0e50d9fd5f69b
KenHV <yo@kenharris.xyz>
2022-01-31 17:02:27 +0530

    sm6150-common: Drop libperfmgr and revert to QTI perf HAL
    Revert back to 56d5d1e444262fb9e2d3b41214cb2c4f1da8c06f.
    
    Signed-off-by: KenHV <yo@kenharris.xyz>

--- common.mk
+++ common.mk
@@ -201,8 +201,8 @@ PRODUCT_PACKAGES += \
     init.qcom.crashdata.sh \
     init.qcom.early_boot.sh \
     init.qcom.efs.sync.sh \
+    init.qcom.post_boot.sh \
     init.qcom.rc \
-    init.qcom.power.rc \
     init.qcom.sdio.sh \
     init.qcom.sensors.sh \
     init.qcom.sh \
@@ -399,7 +399,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service.sm6150-libperfmgr
+    android.hardware.power@1.2-service-qti
 
 # QCOM
 PRODUCT_COPY_FILES += \
@@ -465,9 +465,7 @@ PRODUCT_PACKAGES += \
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
-	$(LOCAL_PATH) \
-    hardware/google/interfaces \
-    hardware/google/pixel
+	$(LOCAL_PATH)
 
 # Speed profile services and wifi-service to reduce RAM and storage
 PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
