7fa08ca4df7f66d3ce366dbfa10fb69aa26eca5b
ZIDAN44 <zidan.roking@gmail.com>
2022-04-22 19:55:37 +0000

    sm6150-common: Guard qti thermal 2.0 service hals
    * Its crashes on liber but fine on other devices
    
     * Crash:
        - E android.hardware.thermal@2.0-service.qti: ThermalHAL not initialized properly.
        - E ThermalHalWrapper: Couldn't get temperatures because of HAL error: ThermalHAL not initialized properly.
    
    Change-Id: Idabe84f3f116fba27e4e9c6e5a9f6e840db20041

--- common.mk
+++ common.mk
@@ -501,8 +501,10 @@ PRODUCT_PACKAGES += \
     TetheringConfigOverlay
 
 # Thermal
+ifeq ($(TARGET_USE_QTI_THERMAL_SERVICE),true)
 PRODUCT_PACKAGES += \
     android.hardware.thermal@2.0-service.qti
+endif
 
 # Update engine
 PRODUCT_PACKAGES += \
