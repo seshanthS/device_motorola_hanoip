ff917d5e0c9d687ec126078485212aac37d9cb71
Jabiyeff <cebiyevanar@gmail.com>
2022-07-17 23:40:53 +0530

    hanoip: build with clang 11.0.1

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -65,6 +65,8 @@ BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_SOURCE := kernel/motorola/hanoip
 TARGET_KERNEL_CLANG_COMPILE := true
+TARGET_KERNEL_CLANG_VERSION := r383902
+TARGET_KERNEL_CLANG_PATH := $(shell pwd)/prebuilts/clang/host/linux-x86/clang-r383902
 TARGET_KERNEL_ADDITIONAL_FLAGS := \
     HOSTCFLAGS="-fuse-ld=lld -Wno-unused-command-line-argument"
 
